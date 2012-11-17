dbe9153ca70caa4bbfc30e266ad188d47630382c
Philip John <phil@philipjohn.co.uk>
2012-11-17 15:48:17 +0000

    Added logo

--- header.php
+++ header.php
@@ -41,10 +41,10 @@
 				<div id="inner-header" class="wrap clearfix">
 					
 					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri().'/library/images/logo_320.png'; ?>" alt="<?php bloginfo('name'); ?>" /></a></p>
 					
 					<!-- if you'd like to use the site description you can un-comment it below -->
-					<?php // bloginfo('description'); ?>
+					<p class="site-description"><?php bloginfo('description'); ?></p>
 					
 					
 					<nav role="navigation">
